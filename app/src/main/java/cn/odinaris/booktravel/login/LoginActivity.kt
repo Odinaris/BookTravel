79c52539654c1ddc3322b09037399302e19a4319
Odinaris <1026250255@qq.com>
2017-04-13 11:27:10 +0800

    增加登陆ViewPager布局文件

--- app/src/main/java/cn/odinaris/booktravel/login/LoginActivity.kt
+++ app/src/main/java/cn/odinaris/booktravel/login/LoginActivity.kt
@@ -3,13 +3,17 @@ package cn.odinaris.booktravel.login
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
+import android.view.WindowManager
 import cn.odinaris.booktravel.R
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //隐藏标题栏
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        //隐藏状态栏
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.act_login)
         initData()
         initView()
