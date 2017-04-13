467e70e5d099d1d55677ff35942d29e9cecd3a72
Odinaris <1026250255@qq.com>
2017-04-13 14:03:55 +0800

    增加启动页SplashActivity

---
+++ app/src/main/java/cn/odinaris/booktravel/main/SplashActivity.kt
@@ -0,0 +1,61 @@
+package cn.odinaris.booktravel.main
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import cn.bmob.v3.Bmob
+import cn.odinaris.booktravel.MainActivity
+import cn.odinaris.booktravel.R
+import kotlinx.android.synthetic.main.act_splash.*
+
+class SplashActivity : Activity() {
+
+    private var bmp: Bitmap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_splash)
+        Bmob.initialize(this, "1e664726dca54fa3ba5667e97e25e9ed")
+        initImage()
+    }
+
+    private fun initImage() {
+        bmp = readBitMap(R.drawable.wallpaper)
+        iv_start.setImageBitmap(bmp)
+        //设置缩放动画
+        val animation = AnimationUtils.loadAnimation(this@SplashActivity, R.anim.anim_start)
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+                //可以在这里先进行某些操作
+            }
+            override fun onAnimationEnd(animation: Animation) {
+                startActivity()
+            }
+            override fun onAnimationRepeat(animation: Animation) { }
+        })
+        iv_start.startAnimation(animation)
+    }
+
+    private fun startActivity() {
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finish()
+        overridePendingTransition(R.anim.scale_in, R.anim.scale_out)
+    }
+
+    private fun readBitMap(resId: Int): Bitmap {
+        val opt = BitmapFactory.Options()
+        opt.inPreferredConfig = Bitmap.Config.RGB_565
+        val `is` = resources.openRawResource(resId)
+        return BitmapFactory.decodeStream(`is`, null, opt)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        bmp!!.recycle()
+        System.gc()
+    }
+}
