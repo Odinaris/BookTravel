266935c7b3e69e57f1326e085dc8ede76c960885
Odinaris <1026250255@qq.com>
2017-04-13 20:42:32 +0800

    新增图书信息数据类

---
+++ app/src/main/java/cn/odinaris/booktravel/bean/BookCategory.kt
@@ -0,0 +1,10 @@
+package cn.odinaris.booktravel.bean
+
+import cn.bmob.v3.BmobObject
+
+class BookCategory(): BmobObject(){
+    var name = ""
+    var parentCategory = ""
+    var followNum = 0
+    var bookNum = 0
+}
